31c4c890c948d6959b1ce5870a6a620103e52cab
Andreus121 <anezcandia.121@gmail.com>
2026-07-14 18:48:31 -0900

    Add createSchedulers to create task for each group of messages

--- src/main/java/cl/andreus/announcer/AnnouncerScheduler.java
+++ src/main/java/cl/andreus/announcer/AnnouncerScheduler.java
@@ -1,6 +1,9 @@
 package cl.andreus.announcer;
 
+import io.papermc.paper.threadedregions.scheduler.GlobalRegionScheduler;
 import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 
@@ -12,32 +15,31 @@ import java.util.List;
 public class AnnouncerScheduler {
 
     //atributos
+    private final Announcer plugin;
     private List<MessageGroup> groups;
     private HashMap<String, ScheduledTask> tasks;
-    private final Announcer plugin;
 
     //constructor
     public AnnouncerScheduler(Announcer plugin){
         this.plugin = plugin;//guardar el plugin (usar cosas de paper)
-        this.groups = new ArrayList<>(LoadGroups());//cargar los mensajes
+        this.groups = new ArrayList<>();//cargar los mensajes
+        this.loadGroups();
         this.tasks = new HashMap<>();//preparar los temporizadores
+        this.createSchedulers();
     }
 
     //función encargada de cargar todos los grupos de mensajes
-    public List<MessageGroup> LoadGroups(){
+    public void loadGroups(){
         //cargar el archivo de configuración
         File configFile = new File(plugin.getDataFolder(),"messages.yml");
         //pasar el archivo a formato yml
         YamlConfiguration messagesConfig = YamlConfiguration.loadConfiguration(configFile);
 
-        //preparar la lista con los grupos de mensajes cargados
-        List<MessageGroup> listGroups = new ArrayList<>();
-
         //verificar si el archivo está vacío o mal formado
         ConfigurationSection groupsSection = messagesConfig.getConfigurationSection("groups");
         if (groupsSection == null) {
             plugin.getLogger().warning("Hubo un error al cargar el archivo messages.yml");
-            return listGroups; //retornar lista vacía
+            return;
         }
 
         //cargar la sección groups y cada nombre de grupos de esta misma
@@ -49,14 +51,41 @@ public class AnnouncerScheduler {
             boolean random = messagesConfig.getBoolean("groups."+groupName+".random");
             //crear el nuevo grupo de mensaje
             MessageGroup group = new MessageGroup(groupName, interval, random);
-            //cada string de ese grupo guardarlo en su respectivo objeto
-            messagesConfig.getStringList("groups."+groupName+".messages").forEach(message -> {
+            //guardar todos los mensajes del messages.yml
+            List<String> messagesList = messagesConfig.getStringList("groups."+groupName+".messages");
+            //agregar cada mensaje a su respectivo grupo
+            for (String message : messagesList) {
                 group.addMessage(message);
-            });
+            }
             //agregar el grupo a la lista
-            listGroups.add(group);
+            this.groups.add(group);
         });
+    }
 
-        return listGroups;
+    //función para crear el scheduler de cada grupo de mensajes
+    public void createSchedulers() {
+        //cargar el scheduler global del sv
+        GlobalRegionScheduler globalScheduler = plugin.getServer().getGlobalRegionScheduler();
+
+        //crear un scheduler para cada grupo de mensajes
+        ScheduledTask task; //declarar la tarea
+        for(MessageGroup group : this.groups){
+            //crear la tarea
+            task = globalScheduler.runAtFixedRate(plugin,
+                    scheduledTask -> {
+                        //aquí va el código que se ejecuta cada vez
+                        //obtener el siguiente mensaje que toca mostrar
+                        Component message = Component.text(group.getCurrentMessage());
+                        //mostrar el mensaje
+                        Bukkit.broadcast(message);
+
+                    },
+                    20L, //delay inicial para ejecutar la tarea
+                     20L * group.getInterval() //segundos indicados en el atributo del grupo
+            );
+
+            //agregar la tarea al Hash
+            this.tasks.put(group.getName(),task);
+        }
     }
 }
