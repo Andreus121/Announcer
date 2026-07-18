f0b289533f468192a7633044cbfd99f6a2a626e7
Andreus121 <anezcandia.121@gmail.com>
2026-07-18 10:42:09 -0900

    add cooldown to the command /announcer for players

--- src/main/java/cl/andreus/announcer/AnnouncerCommands.java
+++ src/main/java/cl/andreus/announcer/AnnouncerCommands.java
@@ -3,6 +3,8 @@ package cl.andreus.announcer;
 import io.papermc.paper.command.brigadier.BasicCommand;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 
 //imports de java
 import java.util.*;
@@ -11,6 +13,8 @@ public class AnnouncerCommands implements BasicCommand {
     //guardar el padre para acceder a message.yml
     private final Announcer plugin;
     private final AnnouncerScheduler scheduler;
+    //hashmap para el cooldown de /announcer
+    private final HashMap<UUID, Long> cooldowns = new HashMap<>();
 
     //constructor
     public AnnouncerCommands(Announcer plugin, AnnouncerScheduler scheduler){
@@ -44,6 +48,31 @@ public class AnnouncerCommands implements BasicCommand {
     @Override
     public void execute(CommandSourceStack source, String[] args){
         CommandSender sender = source.getSender(); //quien ejecutó el mensaje
+        Entity executor = source.getExecutor(); //qué entidad ejecutó el comando?
+
+        //si es un jugador, tiene cooldown, verifica el cooldown antes de ejecutar comandos
+        if ((executor instanceof Player player)) {
+            //calcular los cooldowns
+            long currentTime = System.currentTimeMillis();
+            long cooldownTime = plugin.getConfig().getLong("cooldown",3000); //3 segundos por defecto
+
+            //si el jugador ya usó el comando antes
+            if(cooldowns.containsKey(player.getUniqueId())){
+                //obtener la última vez que usó el comando
+                long lasUsed = cooldowns.get(player.getUniqueId());
+                //si todavía no puede usar el comando
+                if(currentTime - lasUsed < cooldownTime){
+                    //tomar el mensaje del config.yml
+                    String message = plugin.getConfig().getString("cooldown.message","Debes esperar {cooldown} para usar el comando");
+                    int secondsLeft = (int) ((cooldownTime - (currentTime-lasUsed))/1000);
+                    message = message.replace("{cooldown}", String.valueOf(secondsLeft));
+                    sender.sendPlainMessage(message);
+                }
+            }
+        }
+
+        //ya no tiene cooldown o no es un usuario el que ejecutó el comando
+
 
         //comprobar si ejecutó /announcer reload
         if(args.length == 1 && args[0].equals("reload")){
